b4dc7dd978137986fd78ee7e5c38d204baa27375
ZenXSin <119043044+ZenXSin@users.noreply.github.com>
2023-09-09 13:42:58 +0800

    a

--- src/example/item.kt
+++ src/example/item.kt
@@ -1,33 +1,10 @@
 package example
 
-import arc.Core
-import arc.Events
-import arc.util.Log
-import arc.util.Time
-import mindustry.game.EventType.ClientLoadEvent
-import mindustry.mod.Mod
-import mindustry.ui.dialogs.BaseDialog
+import mindustry.type.Item
 
-class ExampleJavaMod : Mod() {
-    init {
-        Log.info("Loaded ExampleJavaMod constructor.")
-
-        //listen for game load event
-        Events.on(ClientLoadEvent::class.java) { e: ClientLoadEvent? ->
-            //show dialog upon startup
-            Time.runTask(10f) {
-                val dialog = BaseDialog("frog")
-                dialog.cont.add("behold").row()
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-kotlin-mod-frog")).pad(20f).row()
-                dialog.cont.button("I see") { dialog.hide() }.size(100f, 50f)
-                dialog.show()
-            }
+object Item {
+        private var a: Item? = null
+        fun load() {
+            a = Item("a")
         }
-    }
-
-    override fun loadContent() {
-        Log.info("Loading some example content.")
-    }
 }
-
---
+++ src/example/zxs.java
@@ -0,0 +1,10 @@
+package example;
+
+import mindustry.mod.Mod;
+public class zxs extends Mod{
+    @Override
+    public void loadContent(){
+        Item.load();
+    }
+}
+
