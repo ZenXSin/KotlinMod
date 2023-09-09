5a312b7f5e8714eaeddd058f0238f73c33a12478
ZenXSin <119043044+ZenXSin@users.noreply.github.com>
2023-09-10 00:30:34 +0800

    Update Block.kt

--- src/zinxs/Block.kt
+++ src/zinxs/Block.kt
@@ -3,7 +3,13 @@ package zinxs
 import mindustry.content.Items
 import mindustry.type.Category
 import mindustry.type.ItemStack
+import mindustry.type.LiquidStack
 import mindustry.world.blocks.production.GenericCrafter
+import mindustry.world.draw.DrawDefault
+import mindustry.world.draw.DrawLiquidTile
+import mindustry.world.draw.DrawMulti
+import mindustry.world.draw.DrawRegion
+
 
 object Block {
     var 临时协议签署所: GenericCrafter? = null
@@ -11,18 +17,16 @@ object Block {
     fun load() {
         临时协议签署所 = object : GenericCrafter("临时协议签署所")  {
             init {
+                requirements(Category.crafting, ItemStack.with(Items.copper, 250, Items.lead, 350, Items.graphite, 250, Items.surgeAlloy, 200))
+                craftTime = 1200f
                 size = 2
                 hasPower = true
                 hasItems = true
-                update = true
-                health = 2000
-                description = "基础签署措施，可用签署于小型武器和工厂的建筑协议"
-                outputItem = ItemStack(Item.临时协议, 1)
-                itemCapacity = 25
-                craftTime = 4000f
-                category = Category.effect
-                consumePower(10f)
-                requirements = ItemStack.with(Items.copper, 250, Items.lead, 350, Items.graphite, 250, Items.surgeAlloy, 200)
+                hasLiquids = false
+                rotate = false
+                solid = true
+                consumePower(25f)
+                consumeItem(Item.临时协议)
             }
         }
     }
