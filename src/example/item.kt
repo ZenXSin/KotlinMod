85e14eca13e58d04f02eec5ba437bb37eaee9e01
ZenXSin <119043044+ZenXSin@users.noreply.github.com>
2023-09-09 14:17:41 +0800

    新物品

--- src/example/item.kt
+++ src/example/item.kt
@@ -1,11 +1,19 @@
 package example
 
+import arc.graphics.Color
 import mindustry.type.Item
 
-private var a: Item? = null
+private var 天临协议: Item? = null
 object Item {
     @JvmStatic
         fun load() {
-            a = Item("a")
+        天临协议 = object : Item("天临协议", Color.red){
+            init {
+                alwaysUnlocked = false
+                radioactivity = 0f
+                explosiveness = 0f
+                flammability = 0f
+            }
+        }
     }
 }
