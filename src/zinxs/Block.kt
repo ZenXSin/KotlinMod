d56a9a1263b26e2afe9004f6490d3ef0176072ab
ZenXSin <119043044+ZenXSin@users.noreply.github.com>
2023-09-10 09:54:35 +0800

    Update Block.kt

--- src/zinxs/Block.kt
+++ src/zinxs/Block.kt
@@ -28,7 +28,7 @@ object Block {
                 rotate = false
                 solid = true
                 consumePower(18.8f)
-                outputItems = arrayOf(ItemStack(Item.临时协议, 1))
+                outputItem = ItemStack(Item.临时协议, 1)
                 description = "基础签署措施，可用签署于小型武器和工厂的建筑协议"
             }
         }
@@ -48,11 +48,11 @@ object Block {
                 consumeItems(ItemStack(Items.thorium, 5), ItemStack(Items.sand, 10))
                 consumeLiquid(Liquids.cryofluid, 3f).booster = true
                 liquidCapacity = 10f
-                outputItems = arrayOf(ItemStack(Items.phaseFabric, 5))
+                outputItem = ItemStack(Items.phaseFabric, 5)
                 description = "利用钍在衰变时散发的辐射，快速制造布\n[red]需要使用冷却液来确保不会发生爆炸"
                 updateEffect = Fx.fuelburn
                 craftEffect = Fx.pulverizeMedium
-                drawer = Draw.build(2.6f, 3.5342917f, 0f, 4, 0f, 4f)
+                //drawer = Draw.build(2.6f, 3.5342917f, 0f, 4, 0f, 4f)
             }
         }
     }
