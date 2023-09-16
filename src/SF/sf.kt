445a7be472f65128b249d900836fea4d0a77775b
ZenXSin <119043044+ZenXSin@users.noreply.github.com>
2023-09-16 12:46:08 +0800

    Update sf.kt
    修复开机界面ohno和循环的bug

--- src/SF/sf.kt
+++ src/SF/sf.kt
@@ -29,23 +29,24 @@ class Sf : Mod() {
 }
 
 fun home() {
-    BaseDialog("Wellcome to [red][Saturation firepower]").apply{
+    BaseDialog("[yellow]Wellcome to [red][Saturation firepower]").apply{
         cont.apply{
             image(Core.atlas.find("饱和火力-logo")).pad(20f).row()
-            add("欢迎来到饱和火力！").row()
-            button("更新日志"){ update() }.size(100f, 50f)
-            button("转型日志"){ hide() }.size(100f, 50f)
-            button("退出"){ hide() }.size(100f, 50f)
+            button("[blue]更新日志"){ update()
+                hide()}.size(100f, 50f)
+            button("[blue]转型日志"){ hide() }.size(100f, 50f)
+            button("[orange]退出"){ hide() }.size(100f, 50f)
         }
         show()
     }
 }
 
 fun update() {
-    BaseDialog("更新日志").apply{
+    BaseDialog("[blue]更新日志").apply{
         cont.apply{
             add("这里写日志").row()
-            button("返回"){ home() }.size(100f, 50f)
+            button("[orange]返回"){ home()
+                hide()}.size(100f, 50f)
         }
         show()
     }
