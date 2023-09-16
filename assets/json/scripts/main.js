c47474e5ed112997518008525a8dafc36a878f9b
ZenXSin <119043044+ZenXSin@users.noreply.github.com>
2023-09-16 09:35:41 +0800

    项目准备启动

--- assets/json/scripts/main.js
+++
@@ -1,4 +0,0 @@
-require('qwer');
-require('替换背景');
-require('磁型压缩机');
-require('物品');
--- assets/json/scripts/物品.js
+++
@@ -1,8 +0,0 @@
-function newItem(name) {
-	exports[name] = (() => {
-		let myItem = extend(Item, name, {});
-		return myItem;
-	})();
-}
-newItem("自塑合金")
-newItem("反物质")
